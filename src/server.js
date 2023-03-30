a464d186decd2933c58427feb747d0434c66a4e3
yudhit08 <yudhitrenandra99@gmail.com>
2023-03-30 15:04:21 +0700

    add functional

--- src/server.js
+++ src/server.js
@@ -1,5 +1,21 @@
-const hue = 100
+const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
 
-for (let i = 0; i < 100; i++) {
-    console.log(i, hue)
-}
+const init = async () => {
+	const server = Hapi.server({
+		port: 5000,
+		host: "localhost",
+        routes: {
+            cors: {
+              origin: ['*'],
+            },
+          },
+	});
+
+	server.route(routes);
+
+	await server.start();
+	console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
